feb63eb844e0804802dbbd3f0f99f9ea9666c6d6
magotoolivesource <52517277+magotoolivesource@users.noreply.github.com>
2019-07-27 21:16:05 +0900

    수업중 자료업로드
    수업중 자료업로드

--- MTG_TankGame/MTG_TankGame/InGameTank.cs
+++ MTG_TankGame/MTG_TankGame/InGameTank.cs
@@ -13,20 +13,34 @@ namespace MTG_TankGame
         Stage m_InGameStage = null;
 
         Tank m_MyTank = null;
+        Tank m_EnemyTank = null;
+
+        Tank[] m_TankArray = null;
 
         protected override void Initialze()
         {
             m_InGameStage = new Stage();
             m_InGameStage.InitStage();
 
+            m_TankArray = new Tank[10];
 
 
             Vector2 pos = new Vector2();
             pos.X = 5;
-            pos.Y = 5;
+            pos.Y = 8;
 
             m_MyTank = new Tank();
             m_MyTank.InitTankData(pos);
+
+
+            m_EnemyTank = new Tank();
+            pos.Y = 3;
+            m_EnemyTank.InitTankData(pos);
+
+
+            m_TankArray[0] = m_MyTank;
+            m_TankArray[1] = m_EnemyTank;
+
         }
 
         protected override void LoopDraw()
@@ -35,7 +49,15 @@ namespace MTG_TankGame
 
             m_InGameStage.DrawStage( m_Buffer );
 
-            m_MyTank.DrawTank(m_Buffer);
+
+            //m_MyTank.DrawTank(m_Buffer);
+            //m_EnemyTank.DrawTank(m_Buffer);
+
+            foreach (var tankcls in m_TankArray)
+            {
+                if(tankcls != null)
+                    tankcls.DrawTank(m_Buffer);
+            }
 
         }
 
@@ -44,8 +66,20 @@ namespace MTG_TankGame
             if (m_CurrentKeyInfo == null)
                 return;
 
-            // 소스 로직 적용하기
 
+            E_Driection movedirection = E_Driection.Max;
+            m_MyTank.Move(movedirection);
+
+
+            //foreach (var tankcls in m_TankArray)
+            //{
+            //    tankcls.Move( );
+            //}
+
+            //// 소스 로직 적용하기
+            //m_MyTank.UpdateMove( m_CurrentKeyInfo.Value );
+
+            //m_EnemyTank.UpdateAI();
 
         }
 
--- MTG_TankGame/MTG_TankGame/Tank.cs
+++ MTG_TankGame/MTG_TankGame/Tank.cs
@@ -1,12 +1,23 @@
 ﻿using InGameEngine;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MTG_TankGame
 {
+
+    public enum E_Driection
+    {
+        Right = 0,
+        Down,
+        Left,
+        Up,
+
+        Max
+    }
     class Tank
     {
         public Vector2 CurrentPos;
@@ -23,6 +34,72 @@ namespace MTG_TankGame
         {
             p_buff.Draw("A", CurrentPos.X, CurrentPos.Y);
         }
+
+
+        public void Move(E_Driection p_direction)
+        {
+            switch (p_direction)
+            {
+                case E_Driection.Right:
+                    {
+                        CurrentPos.X++;
+                    }
+                    break;
+                case E_Driection.Down:
+                    {
+                        CurrentPos.Y++;
+                    }
+                    break;
+                case E_Driection.Left:
+                    {
+                        CurrentPos.X--;
+                    }
+                    break;
+                case E_Driection.Up:
+                    {
+                        CurrentPos.Y--;
+                    }
+                    break;
+                case E_Driection.Max:
+                default:
+                    {
+                        //Debug.Write();
+                    }
+                    break;
+            }
+
+        }
+
+        //public void UpdateAI()
+        //{
+        //    CurrentPos.X++;
+        //}
+
+        //public void UpdateMove(ConsoleKeyInfo p_keyinfo )
+        //{
+
+        //    if( p_keyinfo.Key == ConsoleKey.RightArrow)
+        //    {
+        //        CurrentPos.X++;
+        //    }
+
+        //    if (p_keyinfo.Key == ConsoleKey.LeftArrow)
+        //    {
+        //        CurrentPos.X--;
+        //    }
+
+        //    if (p_keyinfo.Key == ConsoleKey.UpArrow)
+        //    {
+        //        CurrentPos.Y--;
+        //    }
+
+        //    if (p_keyinfo.Key == ConsoleKey.DownArrow)
+        //    {
+        //        CurrentPos.Y++;
+        //    }
+
+
+        //}
 
 
     }
